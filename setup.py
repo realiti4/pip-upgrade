ed1f4d228408f9d91dea2f683f68b2d3199db041
Onur <43709619+realiti4@users.noreply.github.com>
2020-10-11 23:20:22 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pip-upgrade-tool",
-    version="0.3.3",
+    version="0.3.4",
     author="Onur Cetinkol",
     author_email="realiti44@gmail.com",
     description="An easy tool for upgrading all of your packages while not breaking dependencies",
@@ -23,5 +23,5 @@ setuptools.setup(
     ],
     python_requires='>=3.6',
     packages=["pip_upgrade"],
-    # install_requires=["pandas", "requests"],
+    install_requires=["packaging"],
 )
