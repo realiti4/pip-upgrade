46c2951facef5efa07c38f2d4323fab4b9a315d9
realiti4 <onurcetinkol@gmail.com>
2020-10-15 12:01:28 +0300

    seperate some of the packages

--- pip_upgrade/tool.py
+++ pip_upgrade/tool.py
@@ -1,25 +1,22 @@
 import os
 import sys
 import json
-import pkg_resources
+# import pkg_resources
 from subprocess import call
 import subprocess
 
-from pkg_resources import get_distribution
-
-from pip_upgrade.version_checker import version_check, min_dependency
-
 from pip._vendor import pkg_resources
+from pip_upgrade.dependencies_base import DependenciesBase
 
 """
     TODO
     - further testing is needed
 """
 
-class PipUpgrade:
+class PipUpgrade(DependenciesBase):
     def __init__(self, args):
+        super(PipUpgrade, self).__init__()
         self.args = args
-        self.self_check = False
         self.packages = [dist.project_name for dist in pkg_resources.working_set]
         self.packages.remove('pip')
 
@@ -29,9 +26,8 @@ class PipUpgrade:
         # Exclude editable and user defined packages
         self.excluded_pkgs = [] if self.args.exclude is None else self.args.exclude        
         if not self.args.local:     # Exclude editable packages
-            self.excluded_pkgs = self.get_packages(editable=True) + self.excluded_pkgs
+            self.excluded_pkgs = self.get_packages(args=['--editable']) + self.excluded_pkgs
 
-        self.importance_list = ['==', '~=', '<', '<=', '>', '>=', '!=']
         self.len = len(self.packages)
         
         self.dict = self.create_dict(self.packages)
@@ -39,13 +35,11 @@ class PipUpgrade:
 
     # Packages info
     
-    def get_packages(self, editable=False):
+    def get_packages(self, args=[]):
         """
             This gets packages from pip, but it might be slower. Maybe use this later.
         """
-        arg_list = [sys.executable, '-m', 'pip', 'list', '--format=json']
-        if editable:
-            arg_list.append('--editable')
+        arg_list = [sys.executable, '-m', 'pip', 'list', '--format=json'] + args  
 
         packages = subprocess.check_output(arg_list)
         packages = packages.decode("utf-8").replace("\n", "")
@@ -82,87 +76,6 @@ class PipUpgrade:
 
     def create_dict(self, packages):
         return {x: [] for x in packages}
-
-    # Dependencies
-
-    def get_dependencies(self):
-        """
-            The main func for getting dependencies and comparing them to output a final list
-        """
-
-        self.retrieve_dependencies()
-
-        be_upgraded = {}
-        self.wont_upgrade = {}
-
-        for pkg_dict in self.outdated:
-            pkg_name = pkg_dict['name']
-            current_version = pkg_dict['version']
-            latest_version = pkg_dict['latest_version']
-
-            deps = self.dict[pkg_name]
-
-            apply_dep = self.compare_deps(pkg_name, deps, latest_version)
-
-            be_upgraded[pkg_name] = apply_dep
-
-            # TODO Check if it can be upgraded
-            if len(apply_dep) > 0:
-                version_ = apply_dep[0][1]
-                sign_ = apply_dep[0][0]
-                if not version_check(apply_dep[0][1], latest_version, apply_dep[0][0]):
-                    self.wont_upgrade[pkg_name] = sign_ + version_
-
-        return be_upgraded
-
-    def compare_deps(self, pkg_name, deps, latest_version):
-        """
-            Compares dependencies in a list and decides what packages' final version should be
-        """
-        store = []
-        done = False
-        
-        for i in self.importance_list:
-            for index, i_dep in enumerate(deps):
-                sign, version = i_dep
-
-                # for i in self.importance_list:
-                if sign == i:
-                    store.append([sign, version])
-                    done = True
-                    # return store
-
-            if done:
-                if len(store) > 1:
-                    # TODO check if taking min always is the right thing here
-                    return [min_dependency(store)]
-                else:
-                    return store
-        return store
-
-    def retrieve_dependencies(self):
-        for pkg_main in self.packages:
-
-            try:
-                dep_list = pkg_resources.working_set.by_key[pkg_main].requires()
-            except:
-                dep_list = pkg_resources.working_set.by_key[pkg_main.lower()].requires()
-
-            for i in dep_list:
-                name = i.name        # Name of dependency
-                specs = i.specs     # Specs of dependency
-                
-                if len(specs) != 0:
-                    try:
-                        if len(self.dict[name]) > 0:
-                            self.dict[name].append(specs)
-                        else:
-                            self.dict[name] = specs
-                    except:
-                        if name == 'pillow':    # TODO fix lower case completely
-                            self.dict['Pillow'] = specs
-                        else:
-                            print(f'Skipping {name}, warning: Name mismatch. This will be improved. Manually upgrade if needed')
 
     # Upgrade
 
