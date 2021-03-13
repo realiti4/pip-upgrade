62b06b2e9943c6d600dfff1755efcffd6ee57d3a
realiti4 <onurcetinkol@gmail.com>
2021-03-13 13:26:48 +0300

    option to clear pip cache

--- pip_upgrade/main.py
+++ pip_upgrade/main.py
@@ -1,4 +1,6 @@
 import sys
+import subprocess
+import shutil
 import argparse
 
 from pip_upgrade.tool import PipUpgrade
@@ -7,6 +9,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-e', '--exclude', nargs='+', help="Exclude packages you don't want to upgrade")
 parser.add_argument('--local', action='store_true', help="Upgrades local packages as well")
 parser.add_argument('--novenv', action='store_true', help="Disables venv check")
+parser.add_argument('--clear', action='store_true', help="Clears pip's cache")
 parser.add_argument('-q', '--query', help="Query package dependency info from pypi")
 
 args = parser.parse_args()
@@ -19,8 +22,25 @@ def check_venv():
     if not args.novenv:
         assert not sys.prefix == sys.base_prefix, 'Please use pip-upgrade in a virtualenv. If you would like to surpass this use pip-upgrade --novenv'
 
+def clear_cache():
+    """
+        Clears pip cache
+    """
+    arg_list = [sys.executable, '-m', 'pip', 'cache', 'dir']
+    output = subprocess.check_output(arg_list)
+    output = output.decode("utf-8").replace("\n", "").replace("\r", "")    
+
+    try:
+        shutil.rmtree(output)
+        print('Cache is cleared..')
+    except Exception as e:
+        print(e)
+
 def main():
     check_venv()
+    
+    if args.clear:
+        return clear_cache()
 
     pip_upgrade = PipUpgrade(args)
 
